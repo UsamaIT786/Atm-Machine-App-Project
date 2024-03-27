9a154908a1f9579ef992e2aea189d305f1d5340d
Usama Muzammil <160235187+UsamaIT786@users.noreply.github.com>
2024-03-27 08:24:57 -0700

    Add files via upload

---
+++ Main.ts
@@ -0,0 +1,58 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+
+let mypin = 9798;
+let mybalance = 10000
+
+ let pinAnswer = await inquirer.prompt([
+
+{ type: "number",
+message: "Enter Your Pin",
+name: "pin"
+
+},
+
+])
+console.log(pinAnswer);
+
+if(pinAnswer.pin === mypin){
+    console.log("Correct Pin Code" !!)
+}
+
+else{
+    console.log("Incorrect Pin Code"!!)
+}
+
+let operationans = await inquirer.prompt([
+
+{
+name: "operations",
+type: "list",
+mesege: "Please select option",
+choices: ["withdrawl" , "check balance"]
+
+}
+
+]);
+
+if(operationans.operations === "withdrawl"){
+    let amountanswer = await inquirer.prompt([
+{
+    name: "Amount",
+    message: "Enter Your Amount",
+    type: "number",
+}
+
+
+    ])
+    
+    console.log(operationans)
+    mybalance -= amountanswer.Amount;
+    console.log("your remaining balance is:"+ mybalance)
+
+}
+else if(operationans.operations === "check balance  "){
+    console.log("your remaining balance is: " + mybalance )
+}
+
+
